c31c8129f0f993cb34df1ba17a9e72ebf043f7d7
AZIS William <azis.william@gmail.com>
2020-12-16 01:06:04 +0100

    create new java class prm

---
+++ src/com/osb/william/domain/prm.java
@@ -0,0 +1,24 @@
+package com.osb.william.domain;
+
+public class Prm {
+    public Prm(Integer prmId, Integer prmCode) {
+        this.prmId = prmId;
+        this.prmCode = prmCode;
+    }
+}
+    public Integer getPrmId() {
+        return prmId;
+    }
+
+    public void setPrmId(Integer prmId) {
+        this.prmId = prmId;
+    }
+
+    public Integer getPrmCode() {
+        return prmCode;
+    }
+
+    public void setPrmCode(Integer prmCode) {
+        this.prmCode = prmCode;
+    }
+
